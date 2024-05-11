831e620c51daf500f93f2d2d22b2ac04be2aa754
Supriya Sutar <supriyasutar543@gmail.com>
2024-05-11 15:06:32 +0530

    use of angular material

--- src/app/welcome/welcome.component.ts
+++ src/app/welcome/welcome.component.ts
@@ -1,14 +1,11 @@
 import { Component, OnInit } from '@angular/core';
-
-import {FormsModule} from '@angular/forms';
-
 @Component({
   selector: 'app-welcome',
   templateUrl: './welcome.component.html',
   styleUrls: ['./welcome.component.css']
 })
 export class WelcomeComponent implements OnInit {
-color: string ='#eeeeee';
+
 
  
 
